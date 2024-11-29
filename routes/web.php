c5b9f9b63075f9a74411d5a5adb22b90f42cdecf
arolroddPruebas <antonioroldanprueba12@gmail.com>
2024-11-29 13:19:02 +0100

    commit try view prueba

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome to laravel';
+   return view('welcome');
 });
 
 
 Route::get('/post',function () {
-    return 'post';
+    return view('prueba');
+});
+
+Route::get('/post/{post}', function($post){
+    return $post;
+});
+
+Route::get('/post/{post}/{comment}', function($post,$comment){
+    return "el post es {$post} y la funcion es {$comment}";
 });
